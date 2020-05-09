da3201d2c9dc491a6be21f6efc616794c7a948ed
Bikash Das <bikashdaseee@gmail.com>
2020-05-10 00:46:28 +0600

    Accessing dom element via 'ViewChild' decorator.

--- src/app/cock-pit/cock-pit.component.ts
+++ src/app/cock-pit/cock-pit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { Server } from '../server.model';
 
 @Component({
@@ -10,20 +10,22 @@ export class CockPitComponent implements OnInit {
 
   @Output() addItemEvent = new EventEmitter<Server>()
 
-  onAddServer(newServerName:HTMLInputElement,newServerContent:HTMLInputElement) {
+  @ViewChild('serverContentInput') newServerContent:ElementRef;
+
+  onAddServer(newServerName:HTMLInputElement) {
 
     this.addItemEvent.emit({
       type: 'server',
       name: newServerName.value,
-      content: newServerContent.value
+      content: this.newServerContent.nativeElement.value
     });
   }
 
-  onAddBlueprint(newServerName:HTMLInputElement,newServerContent:HTMLInputElement) {
+  onAddBlueprint(newServerName:HTMLInputElement) {
     this.addItemEvent.emit({
       type: 'blueprint',
       name: newServerName.value,
-      content: newServerContent.value
+      content: this.newServerContent.nativeElement.value
     });
   }
 
